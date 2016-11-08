3a358432363277d65c6ea1e9c920a6e4bb555a08
TairanHu <1796754043@qq.com>
2016-11-08 11:59:20 +0800

    LeetCode028 Implement strStr()

---
+++ LeetCode028_Implement_strStr.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(haystack.find(needle) == string::npos)
+        {
+            return -1;
+        }
+        else
+        {
+            return (int)(haystack.find(needle));
+        }
+    }
+};
