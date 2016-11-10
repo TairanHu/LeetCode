9883e3886ca3c5fbb7ab2f9d48f2de5fa95198b6
TairanHu <1796754043@qq.com>
2016-11-10 14:25:37 +0800

    LeetCode039 Combination Sum

--- LeetCode039_Combination_Sum.cpp
+++ LeetCode039_Combination_Sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        
+
         vector<vector<int>> result;
         vector<int> tmp;
         sort(candidates.begin(), candidates.end());
-        
+
         //for(int i = 0; i < candidates.size(); i++)
         {
             tmp.clear();
             get_result(candidates, target, 0, result, tmp);
         }
-        
+
         return result;
-        
+
     }
-    
+
     void get_result(vector<int>& candidates, int target, int pos, vector<vector<int>>& result, vector<int>& tmp)
     {
         if(target == 0)
@@ -23,15 +23,15 @@ public:
             result.push_back(tmp);
             return;
         }
-        
+
         for(int i = pos; i < candidates.size() && candidates[i] <= target; i++ )
-        {   
-            //²»¿¼ÂÇÖØ¸´ÔªËØ
+        {
+            ////ä¸è€ƒè™‘é‡å¤å…ƒç´ 
             tmp.push_back(candidates[i]);
             get_result(candidates, target-candidates[i], i, result, tmp);
             tmp.pop_back();
         }
-        
+
     }
-    
+
 };
