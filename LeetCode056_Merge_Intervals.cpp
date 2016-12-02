535d85b594faf511d82e8329c40d5d1c38607e8b
TairanHu <wit_hutairan@163.com>
2016-12-03 02:07:22 +0800

    LeetCode056 Merge Intervals

---
+++ LeetCode056_Merge_Intervals.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+
+    static bool cmp(const Interval& a, const Interval& b)
+    {
+        return a.start < b.start;
+    }
+
+    vector<Interval> merge(vector<Interval>& intervals) {
+        vector<Interval> result;
+        struct Interval tmp(1,5);
+        
+        if(intervals.size() == 0)
+        {
+            return result;
+        }
+        
+        sort(intervals.begin(), intervals.end(), cmp);
+        result.push_back(intervals[0]);
+        
+        for(int i = 1; i < intervals.size(); i++)
+        {
+            if(intervals[i].start <= result.back().end)
+            {
+                tmp.start = result.back().start;
+                tmp.end = max(intervals[i].end, result.back().end);
+                result.pop_back();
+                result.push_back(tmp);
+            }
+            else
+            {
+                result.push_back(intervals[i]);
+            }
+        }
+        
+        return result;
+    }
+};
