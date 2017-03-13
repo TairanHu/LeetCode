2cc21b547ffae6ab54d5e11fa45f58f6ddb9b01d
TairanHu <wit_hutairan@163.com>
2017-03-14 01:41:09 +0800

    LeetCode122 Best Time to Buy and Sell Stock II

--- LeetCode122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ LeetCode122_Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -20,3 +20,22 @@ public:
         
     }
 };
+
+
+
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        
+        int res = 0;
+        
+        for(int i = 1; i < prices.size(); i++)
+        {
+            res += prices[i]>prices[i-1] ? prices[i]-prices[i-1] : 0;
+        }
+        
+        return res;
+        
+    }
+};
