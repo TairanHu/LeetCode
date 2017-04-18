f782e34a32fbf38d7c6e53aad718bfac4a07b868
TairanHu <wit_hutairan@163.com>
2017-04-18 18:17:51 +0800

    LeetCode342 Power of Four

---
+++ LeetCode342_Power_of_Four.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        
+        if(num <= 0)
+        {
+            return false;
+        }
+        
+        return !(num&(num-1)) && (num&0x55555555);
+        
+    }
+};
