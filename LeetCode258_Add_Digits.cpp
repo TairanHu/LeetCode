6d050e722714ebb8ebf1494c71678560d20300a6
TairanHu <wit_hutairan@163.com>
2017-04-19 01:25:49 +0800

    LeetCode258 Add Digits

---
+++ LeetCode258_Add_Digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        if(num == 0)
+        {
+            return 0;
+        }
+        
+        return ((num-1)%9 + 1);
+        
+    }
+};
