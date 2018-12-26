8062561fd53e0e351d15893160f3852cb0e191ab
胡泰然 <wit_hutairan@163.com>
2018-12-26 20:48:57 +0800

    LeetCode9 Palindrome Number

---
+++ LeetCode009_Palindrome_Number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        if((x < 0) || (x!=0 && (x%10 == 0)))
+        {
+            return false;
+        }
+        
+        int sum = 0;
+        while(x > sum)
+        {
+            sum = sum*10 + x%10;
+            x = x/10;
+        }
+        
+        if((x == sum) || (x == sum/10))
+            return true;
+        return false;
+        
+    }
+};
