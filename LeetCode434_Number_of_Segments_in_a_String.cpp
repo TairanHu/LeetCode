dfadc4bc1938e843a7edee0f6b91b844c6e9ca14
胡泰然 <wit_hutairan@163.com>
2019-01-27 19:59:40 +0800

    Create LeetCode434_Number_of_Segments_in_a_String.cpp

---
+++ LeetCode434_Number_of_Segments_in_a_String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countSegments(string s) {
+        int res = 0;        
+        s = ' ' + s;
+        for(int i = 1; i < s.size(); i++)
+        {
+            if(s[i] != ' ' && s[i-1] == ' ')
+                res++;
+        }        
+        return res;
+    }
+};
